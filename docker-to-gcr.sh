5178856627ff3a98b8faa77d647eff96899422e8
Carl Camit <carlcamit0@gmail.com>
2022-03-21 03:46:15 +1100

    Create Github Action Workflow

--- docker-to-gcr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-IMAGE_NAME="$INPUT_GCR_HOST/$GOOGLE_PROJECT_ID/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TAG"
-
-echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
-docker pull dfranciswoolies/ciarecruitment-bestapiever:247904
-docker tag $IMAGE_NAME
-docker push $IMAGE_NAME
